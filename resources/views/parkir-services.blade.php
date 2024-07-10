675291baf16d7e6878dff79997137e47b1f3f909
IlhamArifinnn <arifinn2208@gmail.com>
2024-07-11 05:53:55 +0700

    add landing page

--- resources/views/parkir-services.blade.php
+++ resources/views/parkir-services.blade.php
@@ -6,9 +6,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800&display=swap"
-        rel="stylesheet">
-
     @include('layouts.links')
 
 </head>
